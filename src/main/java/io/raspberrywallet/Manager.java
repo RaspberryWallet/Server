b4f42d3bffd01b7fcdc8cd73958d2aa5a546c236
stasbar <stasbar1995@gmail.com>
2018-06-21 14:18:28 +0200

    new interface protocol methods
    Took 8 minutes

--- src/main/java/io/raspberrywallet/Manager.java
+++ src/main/java/io/raspberrywallet/Manager.java
@@ -10,6 +10,9 @@ import java.util.List;
 public interface Manager {
     String ping(); // for DEBUG purposes
 
+    /*
+     * Modules domain
+     */
     /**
      * @return all available modules
      */
@@ -22,21 +25,32 @@ public interface Manager {
 
     /**
      * Validate input for specified module, and receive next Step or Failure
+     *
      * @param moduleId id of corresponding nodule
-     * @param input input for current Step
+     * @param input    input for current Step
      * @return Response with next Step or null if failed
      */
     Response nextStep(@NotNull String moduleId, byte[] input); // pass input for current step and return next step
 
+
+
+
+    /*
+     * Bitcoin domain
+     */
+
     /**
      * @return public address which is base58(publicKey)
      */
-    @NonNls byte[] getAddress();
+    @NonNls
+    byte[] getAddress();
 
     /**
      * Restores private key from backup phrase (12 mnemonic words)
+     *
      * @param mnemonicWords 12 words corresponding to private key
      */
     void restoreFromBackupPhrase(@NotNull List<String> mnemonicWords);
+
 
 }
