60b8cd43b902a198efc12363a05c4921aaf5e0ea
stasbar <stasbar1995@gmail.com>
2018-06-23 23:52:13 +0200

    add buildScripts remove CORS security leak add static react app resources
    Took 54 minutes

--- src/main/kotlin/io/raspberrywallet/server/Server.kt
+++ src/main/kotlin/io/raspberrywallet/server/Server.kt
@@ -3,12 +3,11 @@ package io.raspberrywallet.server
 import com.stasbar.Logger
 import io.raspberrywallet.Manager
 import io.vertx.core.Vertx
-import io.vertx.core.http.HttpMethod
 import io.vertx.core.http.HttpServer
 import io.vertx.ext.web.Route
 import io.vertx.ext.web.Router
 import io.vertx.ext.web.RoutingContext
-import io.vertx.ext.web.handler.CorsHandler
+import io.vertx.ext.web.handler.StaticHandler
 import io.vertx.kotlin.core.json.array
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
@@ -16,7 +15,6 @@ import io.vertx.kotlin.coroutines.CoroutineVerticle
 import io.vertx.kotlin.coroutines.awaitResult
 import io.vertx.kotlin.coroutines.dispatcher
 import kotlinx.coroutines.experimental.launch
-import java.util.*
 
 
 class Server(private val manager: Manager) {
@@ -35,14 +33,13 @@ class Server(private val manager: Manager) {
 internal class MainVerticle(private val manager: Manager) : CoroutineVerticle() {
     override suspend fun start() {
         val router = Router.router(vertx)
-        acceptCORS(router)
-        router.get("/ping").coroutineHandler {
+        router.get("/api/ping").coroutineHandler {
             Logger.d("/ping")
             it.response().end(json {
                 obj("ping" to manager.ping()).encodePrettily()
             })
         }
-        router.get("/modules").coroutineHandler {
+        router.get("/api/modules").coroutineHandler {
             Logger.d("/modules")
             it.response().end(json {
                 array(
@@ -51,41 +48,23 @@ internal class MainVerticle(private val manager: Manager) : CoroutineVerticle() 
 
             })
         }
-        router.get("/moduleState/:id").coroutineHandler {
-
-
+        router.get("/api/moduleState/:id").coroutineHandler {
             it.response().end(json {
                 Logger.d("/moduleState/${it.pathParam("id")}")
                 val moduleState = manager.getModuleState(it.pathParam("id"))
                 obj(
                     "state" to moduleState.name, "message" to moduleState.message
                 ).encodePrettily()
-
             })
         }
+        router.route("/*").handler(StaticHandler.create("assets"))
+
         awaitResult<HttpServer> {
             vertx.createHttpServer()
                 .requestHandler(router::accept)
-                .listen(config.getInteger("http.port", 9090), it)
+                .listen(config.getInteger("http.port", 8080), it)
         }
     }
-}
-
-fun acceptCORS(router: Router) {
-    val allowedHeaders = HashSet<String>()
-    allowedHeaders.add("x-requested-with")
-    allowedHeaders.add("Access-Control-Allow-Origin")
-    allowedHeaders.add("origin")
-    allowedHeaders.add("Content-Type")
-    allowedHeaders.add("accept")
-    allowedHeaders.add("X-PINGARUNER")
-
-    val allowedMethods = HashSet<HttpMethod>()
-    allowedMethods.add(HttpMethod.GET)
-    allowedMethods.add(HttpMethod.POST)
-    allowedMethods.add(HttpMethod.OPTIONS)
-
-    router.route().handler(CorsHandler.create("*").allowedHeaders(allowedHeaders).allowedMethod(HttpMethod.GET))
 }
 
 /**
